0f55214ff10711d2bfdc12b4cfef0c377e09b36d
Jacek Filipowski <jacek.filipowski@ryerson.ca>
2019-04-05 15:15:18 -0400

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,4 +11,20 @@ using RouteBidModel
 
 include("../src/decls.jl")
 
-@test true
+g =  SimpleWeightedDiGraph(5)
+add_edge!(g, 2, 3, 730.0)
+add_edge!(g, 3, 1, 910.0)
+add_edge!(g, 2, 1, 110.0)
+add_edge!(g, 1, 3, 100.0)
+add_edge!(g, 4, 1, 700.0)
+add_edge!(g, 5, 4, 330.0)
+
+nw = Decls.Network(g)
+Decls.InitNetwork!(nw)
+Decls.SetSpawnsAndDests!(nw, [3], [5])
+for i in 1:10
+    Decls.SpawnAgentAtRandom(nw)
+end
+sim = Decls.Simulation(nw, 60, 1, 0, true)
+
+@test length(nw.agents) == 10
