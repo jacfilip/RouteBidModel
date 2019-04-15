af4a21a4667c7240e86e1c63de90d06b738286a4
Jacek Filipowski <jacek.filipowski@ryerson.ca>
2019-04-15 10:55:28 -0400

    missing pkgs added

--- src/RouteBidModel.jl
+++ src/RouteBidModel.jl
@@ -1,8 +1,13 @@
 module RouteBidModel
 
+Pkg.add("OpenStreetMapX")
+Pkg.add("LightGraphs")
+Pkg.add("SimpleWeightedGraphs")
+
 using Test
 using OpenStreetMapX
 using LightGraphs, SimpleWeightedGraphs
+
 
 include("./decls.jl")
 
