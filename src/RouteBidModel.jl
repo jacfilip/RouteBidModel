4c1a7ea6f5413322d376712e2a5e0930d946f012
Jacek Filipowski <jacek.filipowski@ryerson.ca>
2019-04-15 11:29:39 -0400

    pkg fix

--- src/RouteBidModel.jl
+++ src/RouteBidModel.jl
@@ -2,9 +2,14 @@ module RouteBidModel
 
 using Pkg
 
-Pkg.add("OpenStreetMapX")
 Pkg.add("LightGraphs")
 Pkg.add("SimpleWeightedGraphs")
+Pkg.add("OpenStreetMapX")
+Pkg.add("DataFrames")
+Pkg.add("DataFramesMeta")
+Pkg.add("Distributions")
+Pkg.add("CSV")
+Pkg.add("Compose")
 
 using Test
 using OpenStreetMapX
