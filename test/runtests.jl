83b0c974b502a305ada40a8299bc826829a05532
Arash Dehghan <arash.dehghank@gmail.com>
2019-05-03 03:59:37 -0400

    Update runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,20 +24,26 @@ using CSV
 using DelimitedFiles
 using SparseArrays
 using Random
+using Conda
+using PyCall
 
 include("../src/decls.jl")
-include("../src/Visuals.jl")
 include("../src/osm_convert.jl")
+# include("../src/Visuals.jl")
 
 Random.seed!(0)
 
-nw = CreateNetworkFromFile("maps", "buffaloF.osm")
+path = "/Users/arashdehghan/Desktop/RouteBidModel/maps/"
+file = "buffaloF.osm"
+
+nw = CreateNetworkFromFile(path, file)
 Decls.SetSpawnAndDestPts!(nw, Decls.GetNodesOutsideRadius(nw,(-2000.,-2000.),4000.), Decls.GetNodesInRadius(nw,(-2000.,-2000.),2000.))
 
-sim = Decls.Simulation(nw, 100.0, maxAgents = 1000, dt = 5.0)
+sim = Decls.Simulation(nw, 350.0, maxAgents = 100, dt = 5.0)
 
 @time Decls.RunSim(sim)
 
 CSV.write(raw".\results\history.csv", sim.simData)
-CSV.write(raw".\results\coords.csv", Decls.GetIntersectionCoords2(sim.network))
+CSV.write(raw".\results\roadInfo.csv", sim.roadInfo)
+CSV.write(raw".\results\coords.csv", Decls.GetIntersectionCoords(sim.network))
 writedlm(raw".\results\log.txt", Decls.simLog, "\n")
