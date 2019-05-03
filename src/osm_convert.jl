f272388f744d46af68863e369b8a7c26e963a2d9
Arash Dehghan <arash.dehghank@gmail.com>
2019-05-03 03:57:40 -0400

    updated osm converter

--- src/osm_convert.jl
+++ src/osm_convert.jl
@@ -6,22 +6,11 @@ using GraphPlot
 using Plots
 using Compose, Colors
 
-#path = raw"C:\Users\jacek.filipowski\github\route-bid-model\maps"
-#mapName = "buffaloF.osm"
-
 function CreateNetworkFromFile(path::String, mapName::String)::Decls.Network
-    map = OpenStreetMapX.parseOSM(path * "\\" * mapName)
+    map = OpenStreetMapX.parseOSM(path * mapName)
     crop!(map)
 
-    mData = get_map_data(path, mapName, only_intersections = true, use_cache = false)
+    mData = get_map_data(path, mapName, only_intersections = true)
     nw = Decls.Network(mData)
-    #mapPlot = OpenStreetMapXPlot.plotmap(mData,width=1000,height=800)
     return nw
 end
-
-#map = OpenStreetMapX.parseOSM(raw"C:\Users\jacek.filipowski\github\route-bid-model\maps\buffaloF.osm")
-#crop!(map)
-#mData = get_map_data(path, mapName, only_intersections = true)
-
-map = OpenStreetMapX.parseOSM("maps" * "\\" * "buffaloF.osm")
-mData = get_map_data("maps", "buffaloF.osm", only_intersections = true)
