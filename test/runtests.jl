08243948b2f65ae603edc88b9b5179b06a2e12ff
Arash Dehghan <arash.dehghank@gmail.com>
2019-05-03 15:11:45 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -29,7 +29,7 @@ using PyCall
 
 include("../src/decls.jl")
 include("../src/osm_convert.jl")
-# include("../src/Visuals.jl")
+include("../src/Visuals.jl")
 
 Random.seed!(0)
 
@@ -39,11 +39,18 @@ file = "buffaloF.osm"
 nw = CreateNetworkFromFile(path, file)
 Decls.SetSpawnAndDestPts!(nw, Decls.GetNodesOutsideRadius(nw,(-2000.,-2000.),4000.), Decls.GetNodesInRadius(nw,(-2000.,-2000.),2000.))
 
-sim = Decls.Simulation(nw, 350.0, maxAgents = 100, dt = 5.0)
+sim = Decls.Simulation(nw, 300.0, maxAgents = 100, dt = 5.0)
 
 @time Decls.RunSim(sim)
 
-CSV.write(raw".\results\history.csv", sim.simData)
-CSV.write(raw".\results\roadInfo.csv", sim.roadInfo)
-CSV.write(raw".\results\coords.csv", Decls.GetIntersectionCoords(sim.network))
-writedlm(raw".\results\log.txt", Decls.simLog, "\n")
+map = OpenStreetMapX.parseOSM(path * file)
+mData = get_map_data(path,file,only_intersections = true)
+
+Visuals.OVAGraph(map,mData,sim.agentsFinished[1])
+Visuals.GraphAgents(map,mData,sim.agentsFinished)
+
+
+CSV.write("/Users/arashdehghan/Desktop/RouteBidModel/results/history.csv", sim.simData)
+CSV.write("/Users/arashdehghan/Desktop/RouteBidModel/results/roadInfo.csv", sim.roadInfo)
+CSV.write("/Users/arashdehghan/Desktop/RouteBidModel/results/coords.csv", Decls.GetIntersectionCoords(sim.network))
+writedlm("/Users/arashdehghan/Desktop/RouteBidModel/results/log.txt", Decls.simLog, "\n")
