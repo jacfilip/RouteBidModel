9079c28d578d3320042d81d9c1a5f430277558ae
jfilipowski1989 <jfilipowski1989@gmail.com>
2020-01-11 00:37:26 +0100

    two-roads scenario added

--- src/RouteBidModel.jl
+++ src/RouteBidModel.jl
@@ -53,35 +53,36 @@ cd("C:\\Users\\jacekf\\github\\RouteBidModel\\src")
 nw = CreateNetworkFromFile(path * "//", file)
 #SetSpawnAndDestPts!(nw, GetNodesOutsideRadius(nw,(-2000.,-2000.),4000.), GetNodesInRadius(nw,(-2000.,-2000.),2000.))
 
-#40.6945, 40.7163, -74.0019, -73.9738 X=2367m Y=2422
-#SetSpawnAndDestPts!(nw, GetNodesBetween(nw,(-700.,-700.), 1000, 2000), GetNodesInRadius(nw,(-700.,-1500.), 500.))
-#SetSpawnAndDestPts!(nw, GetNodesInRadius(nw,(700.,-1500.), 500.), GetNodesInRadius(nw,(-1500., 1500.), 500.))
-
 #Bridge1: 1044->1045, 532->1214 , 445->446
-SetSpawnAndDestPts!(nw, [345], [847])
-
+#SetSpawnAndDestPts!(nw, [345], [958])
+inter = GetIntersectByNode(nw, 553)
+SetSpawnAndDestPts!(nw,  GetNodesInRadius(nw, (inter.posX, inter.posY), 200), [847])
+#disable lane no.1
 GetRoadByNodes(nw, 1044, 1045).length = Inf
 nw.graph.weights[1044, 1045] = Inf
 
+#disable lane no.2
 GetRoadByNodes(nw, 532, 1214).length = Inf
 nw.graph.weights[532, 1214] = Inf
 
+#disable lane no.3
 GetRoadByNodes(nw, 445, 446).length = Inf
 nw.graph.weights[445, 446] = Inf
 
 
 Random.seed!(0)
-sim = Simulation(nw, 30 * 60, maxAgents = 2000, dt = 5.0, initialAgents = 2000, auctions = false)
+sim = Simulation(nw, 30 * 60, maxAgents = 10000, dt = 5.0, initialAgents = 8000, auctions = false)
 @time RunSim(sim)
 SaveSim(sim, "sim_na_2k_t=90m")
-CSV.write(raw"C:\Users\jacek.filipowski\Dropbox\Canada2019\results\simulations" * "\\" * "manh_na_90m_1_agent_auctions.csv", DumpAuctionsInfo(sim))
-CSV.write(raw"C:\Users\jacekf\Dropbox\Canada2019\results\simulations" * "\\" * "manh_na_90m_1_agent_finished.csv", DumpFinishedAgents(sim))
 
-CSV.write(raw".\results\history.csv", sim.simData)
-CSV.write(raw".\results\roadInfo.csv", sim.roadInfo)
-CSV.write(raw".\results\auctions.csv", DumpAuctionsInfo(sim))
-CSV.write(raw".\results\finished.csv", DumpFinishedAgents(sim))
-writedlm(raw".\results\log.txt", simLog, "\n")
+#CSV.write(raw"C:\Users\jacek.filipowski\Dropbox\Canada2019\results\simulations" * "\\" * "manh_na_90m_1_agent_auctions.csv", DumpAuctionsInfo(sim))
+#CSV.write(raw"C:\Users\jacekf\Dropbox\Canada2019\results\simulations" * "\\" * "manh_na_90m_1_agent_finished.csv", DumpFinishedAgents(sim))
+
+#CSV.write(raw".\results\history.csv", sim.simData)
+#CSV.write(raw".\results\roadInfo.csv", sim.roadInfo)
+#CSV.write(raw".\results\auctions.csv", DumpAuctionsInfo(sim))
+#CSV.write(raw".\results\finished.csv", DumpFinishedAgents(sim))
+#writedlm(raw".\results\log.txt", simLog, "\n")
 
 
 osmmap = OpenStreetMapX.parseOSM(path * "//" * file)
